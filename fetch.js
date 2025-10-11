c7bbff25c0e655007fe1e4b7bab7a36ec91c5c40
Patiphat <youaremyhopes@gmail.com>
2025-10-11 23:56:12 +0700

    dd

--- fetch.js
+++ fetch.js
@@ -19,5 +19,5 @@ fetch('https://jsonplaceholder.typicode.com/posts')
     })
 
     .catch(error => {
-        console.log('error fetching data' , error)
+        console.log('err fetching data' , error)
     })
