7416e11926433fa840160fab2317e501208340c3
Patiphat <youaremyhopes@gmail.com>
2025-10-18 23:53:44 +0700

    home

--- fetch.js
+++ fetch.js
@@ -19,5 +19,5 @@ fetch('https://jsonplaceholder.typicode.com/posts')
     })
 
     .catch(error => {
-        console.log('e dsdsadasdataddd' , error)
+        console.log('e dasdasdadasd' , error)
     })
